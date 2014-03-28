bf907d8bef16f09a65db32df8b828128a9df98b6
nhinkie <nhinkie@islander.tamucc.edu>
2014-03-27 20:26:26 -0500

    Added some comments and a base for a character class
    Character class will be used to fill a list of characters at the start
    of the game. Players will be able to select their character from this
    list at the beginning of the game

---
+++ TheColony/TheColony/TheColony/Character.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace TheColony
+{
+    class Character
+    {
+        #region character's game-related attributes
+
+        public float health;           //character's current health
+        public float damage;           //damage dealt per attack
+        public Vector2 location;       //location of character in world
+        public bool dealsDamage;       //true if character has an attack, else false
+        public bool isUnconscious;     //true when health reachs 0. if true, character cannot be controlled by player
+        public bool isAvailable;       //true when character has yet to be picked by a player, false when character is selected
+
+        #endregion
+
+        #region character's lore-related attributes
+
+        public string name;            //character's name
+        public int age;                //character's age
+        public string birthPlace;      //character's birth place
+        public string occupation;    //character's occupation(s)
+        public string education;     //character's education
+        public string backstory;       //character's backstory
+
+        #endregion
+    }
+}
--- TheColony/TheColony/TheColony/GameScreen.cs
+++ TheColony/TheColony/TheColony/GameScreen.cs
@@ -37,49 +37,67 @@ namespace TheColony
         private Vector2 playerOffset;
         private Vector2 playerNewPosition;
         private Matrix isoProjectionMatrix;
+        private List<Character> characterList;
         
         public GameScreen(Game1 game)
         {
             this.game = game;
+
+            //load textures to be used in game
             tHud = game.Content.Load<Texture2D>("gameHUD");
+            player_temp = game.Content.Load<Texture2D>("radSprite_temp");       //temporary sprite to test movement
+            cursor = game.Content.Load<Texture2D>("Pointer");                   //sprite for cursor
+            radSprite = game.Content.Load<Texture2D>("radSprite");              //radiation sprite sheet(unused currently)
+            background = game.Content.Load<Texture2D>("background");            //image for background
+            large_background = game.Content.Load<Texture2D>("Game_Background"); //temporary background cuz it looks better
+            
+            //currently not used but will probably be eventually
             lastKeyboardState = Keyboard.GetState();
+            //currently not used but will probably be eventually
             lastMouseState = Mouse.GetState();
 
-            camera = new Camera();
+            camera = new Camera();          
             spriteBatch = new SpriteBatch(game.GraphicsDevice);
+
+            //different viewports
             defaultView = game.GraphicsDevice.Viewport;
             gameView = defaultView;
-            gameView.Width = gameView.Width - tHud.Width;
-
-            player_temp = game.Content.Load<Texture2D>("radSprite_temp");
-            cursor = game.Content.Load<Texture2D>("Pointer");
-            radSprite = game.Content.Load<Texture2D>("radSprite");
-            background = game.Content.Load<Texture2D>("background");
-            large_background = game.Content.Load<Texture2D>("Game_Background");
+            gameView.Width = gameView.Width - tHud.Width; //viewport for player's camera
+            
             //cursor = Content.Load<Texture2D>("cursor");
             //fHud = game.Content.Load<SpriteFont>("HudFont");
 
+            #region temporary player attributes just to test player movement 
             playerPosition = new Vector2(700,600);
             playerNewPosition = playerPosition;
             playerOffset = new Vector2(player_temp.Width / 2, player_temp.Height - 10);
             playerPosition -= playerOffset;
+            #endregion 
+
+            //projection Matrix used for uh... I forgot. Will probably remove soon
             isoProjectionMatrix = Matrix.CreatePerspectiveFieldOfView(
                 MathHelper.PiOver4, 2.0f / 3.0f, 1.0f, 10000f);
+
+            //addCharacters();        //Adds characters to the game(unfinished
         }
 
         public void Update()
         {
+            
             key = Keyboard.GetState();
             mouse = Mouse.GetState();
 
             mousePosition = new Vector2(mouse.X, mouse.Y);
 
+            //allows game to exit
             if (key.IsKeyDown(Keys.Escape))
                 game.Exit();
 
+            //allows to toggle fullscreen
             if (key.IsKeyDown(Keys.F))
                 graphics.ToggleFullScreen();
 
+            //moves camera when cursor is near edge of camera's viewport
             if (mouse.X < 50)
                 camera.Pan(new Vector2(-1, 1));
             if (mouse.X > game.GraphicsDevice.DisplayMode.Width - 166 - 50 && mouse.X < game.GraphicsDevice.DisplayMode.Width - 166)
@@ -89,12 +107,17 @@ namespace TheColony
             if (mouse.Y > game.GraphicsDevice.DisplayMode.Height - 50 && mouse.X < game.GraphicsDevice.DisplayMode.Width - 166)
                 camera.Pan(new Vector2(1, 1));
 
+            //gets left mouse click and updates player position
             if (mouse.LeftButton == ButtonState.Pressed)
             {
-                playerNewPosition = mousePosition;
-                playerNewPosition = playerNewPosition - playerOffset;
+                if (mouse.X < game.GraphicsDevice.DisplayMode.Width - 166)
+                {
+                    playerNewPosition = mousePosition;
+                    playerNewPosition = playerNewPosition - playerOffset;
+                }
             }
 
+            //player movement, tobe moved later
             if (Math.Abs(playerPosition.X - playerNewPosition.X) < 2)
             {     // the player is already near his destination
                 playerPosition.X = playerNewPosition.X;
@@ -108,7 +131,7 @@ namespace TheColony
                 playerPosition.Y += Math.Sign(playerNewPosition.Y - playerPosition.Y);
             }
 
-            /*
+            /* old player movement
             if (playerPosition != playerNewPosition)
             {
                 if (playerNewPosition.X - playerPosition.X < 1)
@@ -130,7 +153,7 @@ namespace TheColony
 
         public void Draw()//SpriteBatch spriteBatch)
         {
-            //hud
+            //default viewport, the hud is displayed here
             game.GraphicsDevice.Viewport = defaultView;
             game.GraphicsDevice.Clear(Color.Black);
             spriteBatch.Begin();
@@ -140,6 +163,7 @@ namespace TheColony
             //spriteBatch.DrawString(hud, "HERE", new Vector2(GraphicsDevice.DisplayMode.Width - 250, GraphicsDevice.DisplayMode.Height / 2), Color.White);
             spriteBatch.End();
 
+            //camera's viewport, the game world is displayed here
             game.GraphicsDevice.Viewport = gameView;
             spriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.AlphaBlend, null, null, null, null, camera.getTransformation(game.GraphicsDevice));
             spriteBatch.Draw(large_background, new Vector2(-large_background.Width/2, -large_background.Height/2), Color.White);
@@ -148,11 +172,69 @@ namespace TheColony
             spriteBatch.Draw(player_temp, playerPosition, Color.White);
             spriteBatch.End();
             
+            //back to default viewport to draw cursor on top of everything
             game.GraphicsDevice.Viewport = defaultView;
             spriteBatch.Begin();
             spriteBatch.Draw(cursor, mousePosition, Color.White);
             spriteBatch.End();
             
+        }
+
+
+        //method to add characters to the game
+        private void addCharacters()
+        {
+            //Examples of how to add characters. Info was pulled from trello
+            Character Samantha = new Character();
+            Samantha.name = "Samantha";
+            Samantha.age = 34;
+            Samantha.birthPlace = "Seward, AK";
+            Samantha.occupation = "Teacher";
+            Samantha.education = "Bachelor of Science in Education";
+            Samantha.backstory = "Having grown up the oldest in a large family Samantha loved being around children. " +
+            "Samantha was a great elementary school teacher and spent all her extra time helping underprivileged kids. " +
+            "After the event Samantha has been helping the children.";
+            Samantha.health = 100;
+            Samantha.damage = 0;
+            Samantha.dealsDamage = false;
+            Samantha.isUnconscious = false;
+            Samantha.isAvailable = true;
+            characterList.Add(Samantha);
+
+            Character Richard_B = new Character();
+            Richard_B.name = "Richard Blase";
+            Richard_B.age = 34;
+            Richard_B.birthPlace = "Erie, PA";
+            Richard_B.occupation = "Carpenter";
+            Richard_B.education = "High School Diploma";
+            Richard_B.backstory = "After losing his family to a car accident, Richard gave up on his life long love of carpentry. " +
+            "He walked away from all family and friends taking up a life of solitude. " +
+            "After the event Richard has been securing shelters to defend against thieves and Murders.";
+            Richard_B.health = 100;
+            Richard_B.damage = 0;
+            Richard_B.dealsDamage = false;
+            Richard_B.isUnconscious = false;
+            Richard_B.isAvailable = true;
+            characterList.Add(Richard_B);
+
+            Character Leroy_J = new Character();
+            Leroy_J.name = "Leroy Jenkins";
+            Leroy_J.age = 20;
+            Leroy_J.birthPlace = "Flag Staff, AR";
+            Leroy_J.occupation = "Athlete";
+            Leroy_J.education = "Unknown";
+            Leroy_J.backstory = "Leroy, was a very good athlete with a bright future ahead of him but one unfortunate " +
+            "injury took his career away from him while he was getting ready to join the Olympic Games. " +
+            "He is now trying to prove the world that he is fully recovered and training even harder than before. " +
+            "His athletic body and good health is what keeps him get out of tough situations.";
+            Leroy_J.health = 100;
+            Leroy_J.damage = 0;
+            Leroy_J.dealsDamage = false;
+            Leroy_J.isUnconscious = false;
+            Leroy_J.isAvailable = true;
+            characterList.Add(Leroy_J);
+
+            //Still more characters to add, but can do that later. 
         }
     }
 }
